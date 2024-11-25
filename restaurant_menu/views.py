846367070abcdeb19dc8d34ac4a9e61557309737
FREDYK1 <fkwekukankam@gmail.com>
2024-11-25 23:06:15 +0000

    Create the home view page

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from  .models import Order
 
-# Create your views here.
+class MenuList(generic.ListView):
+    queryset = Order.objects.order_by("date_created")
+    template_name = "home.html"
+
+class MenuItemDetail(generic.DetailView):
+    model = Order
+    template_name = "detail.html"
