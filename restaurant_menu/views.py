8f3e63fff4e16c51edd276ea6b27b00827f9ea08
FREDYK1 <fkwekukankam@gmail.com>
2024-11-30 08:02:23 +0000

    Display Meal_Types, Meal and it's description on the web page

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,12 +1,14 @@
 from django.views import generic
-from  .models import Order
+from  .models import Order, Meal_Types
 
 class MenuList(generic.ListView):
     queryset = Order.objects.order_by("date_created")
     template_name = "home.html"
 
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = {"meals": "Rice"}
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["meals"] = Meal_Types
+        context["Order_list"] = self.queryset
         return context
 
 class MenuItemDetail(generic.DetailView):
